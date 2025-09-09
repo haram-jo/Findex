7e916623cbc77931cdbb546cedc24b488505f977
Soyeon <isylsy166@gmail.com>
2025-09-09 20:43:01 +0900

    chore: 스키마 변경, IndexInfoRepository, pr 템플릿 추가 (#28)

--- src/main/java/com/codeit/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/codeit/findex/repository/IndexInfoRepository.java
@@ -3,9 +3,6 @@ package com.codeit.findex.repository;
 import com.codeit.findex.entity.IndexInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*수정되면 말하기
-*/
-
-// <Entity, PK타입> 상속
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+    boolean existsByIndexClassificationAndIndexName(String indexClassification, String indexName);
 }
