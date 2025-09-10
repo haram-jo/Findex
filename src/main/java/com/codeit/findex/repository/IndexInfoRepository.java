080eeffca5800a94ec2c04eca71c6d4e6413742d
Haram Jo <harramaram@gmail.com>
2025-09-10 10:31:06 +0900

    chore: 불필요한 주석 제거

--- src/main/java/com/codeit/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/codeit/findex/repository/IndexInfoRepository.java
@@ -3,8 +3,6 @@ package com.codeit.findex.repository;
 import com.codeit.findex.entity.IndexInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-// <Entity, PK타입> 상속
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
     boolean existsByIndexClassificationAndIndexName(String indexClassification, String indexName);
 }
